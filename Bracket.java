f7a4778f9554f30e295f440153bc896a30ef4b4d
Connor O'Gara <ogara.connor@gmail.com>
2017-03-02 16:58:35 -0600

    Swing drawing reconstructuring

--- Bracket.java
+++ Bracket.java
@@ -8,19 +8,25 @@ public class Bracket {
     // int numTeams = 8;
     ArrayList<JPanel> panels = new ArrayList<JPanel>();
 
-    public Bracket() {
+    public Bracket(int numRds) {
         // panels = new ArrayList<JPanel>();
         // rounds = new ArrayList<BracketRound>();
-        rounds.add(new BracketRound(1));
-        addRound();
-        addRound();
-        addRound();
-        drawRounds();
+        int numGames = 1;
+        for (int i = 0; i < numRds; i++) {
+            rounds.add(0, new BracketRound(numGames));
+            numGames *= 2;
+        }
+            
+        // addRound();
+        // addRound();
+        // addRound();
+        // drawRounds();
     }
 
-    public void addRound() {
-        int numGames = rounds.get(rounds.size()-1).numGames;
-        rounds.add(new BracketRound(numGames*2));
+    public void addRound(BracketRound round) {
+        rounds.add(0, round);
+        // int numGames = rounds.get(rounds.size()-1).numGames;
+        // rounds.add(new BracketRound(numGames*2));
     }
 
     public void drawRounds() {
@@ -30,9 +36,9 @@ public class Bracket {
             newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.PAGE_AXIS));
             newPanel.setBackground(new Color(255-(i*6),235-(i*5),225-(i*4)));
 
-            Dimension min = new Dimension(250,0);
-            Dimension pref = new Dimension(250,0);
-            Dimension max = new Dimension(250, 0);
+            Dimension min = new Dimension(200,0);
+            Dimension pref = new Dimension(200,0);
+            Dimension max = new Dimension(200, 0);
             newPanel.add(new Box.Filler(min, pref, max));
             
 
@@ -57,15 +63,13 @@ public class Bracket {
             }
             // newPanel.add(Box.createVerticalGlue());
             newPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-            JButton removeButton = new JButton("Remove Round");
-            removeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            newPanel.add(removeButton);
+            
             
             panels.add(newPanel);
         }
     }
 
-
+ 
 
     
 
@@ -121,35 +125,35 @@ public class Bracket {
 
 
 
-class BracketTest {
-    public static void main(String[] args) {
-        Bracket bracket = new Bracket();
-        bracket.addRound(4);
-        bracket.addRound(2);
-        bracket.addRound(1);
-        Team TSM = new Team("TSM");
-        Team CLG = new Team("CLG");
-        Team C9 = new Team("C9");
-        Team EF = new Team("Echo Fox");
-        Team TL = new Team("Team Liquid");
-        Team NV = new Team("Envy");
-        Team DIG = new Team("Dignitas");
-        Team FQ = new Team("Fly Quest");
-        bracket.rounds.get(0).games.get(0).addTeam(TSM);
-        bracket.rounds.get(0).games.get(0).addTeam(CLG);
-        bracket.rounds.get(0).games.get(1).addTeam(C9);
-        bracket.rounds.get(0).games.get(1).addTeam(EF);
-        bracket.rounds.get(0).games.get(2).addTeam(TL);
-        bracket.rounds.get(0).games.get(2).addTeam(NV);
-        bracket.rounds.get(0).games.get(3).addTeam(DIG);
-        bracket.rounds.get(0).games.get(3).addTeam(FQ);
-        bracket.declareWinner(CLG);
-        bracket.declareWinner(C9);
-        bracket.declareWinner(FQ);
-        bracket.declareWinner(NV);
-        bracket.declareWinner(C9);
-        bracket.declareWinner(FQ);
-        bracket.declareWinner(C9);
-        bracket.go();
-    }
-}
+// class BracketTest {
+//     public static void main(String[] args) {
+//         Bracket bracket = new Bracket();
+//         bracket.addRound(4);
+//         bracket.addRound(2);
+//         bracket.addRound(1);
+//         Team TSM = new Team("TSM");
+//         Team CLG = new Team("CLG");
+//         Team C9 = new Team("C9");
+//         Team EF = new Team("Echo Fox");
+//         Team TL = new Team("Team Liquid");
+//         Team NV = new Team("Envy");
+//         Team DIG = new Team("Dignitas");
+//         Team FQ = new Team("Fly Quest");
+//         bracket.rounds.get(0).games.get(0).addTeam(TSM);
+//         bracket.rounds.get(0).games.get(0).addTeam(CLG);
+//         bracket.rounds.get(0).games.get(1).addTeam(C9);
+//         bracket.rounds.get(0).games.get(1).addTeam(EF);
+//         bracket.rounds.get(0).games.get(2).addTeam(TL);
+//         bracket.rounds.get(0).games.get(2).addTeam(NV);
+//         bracket.rounds.get(0).games.get(3).addTeam(DIG);
+//         bracket.rounds.get(0).games.get(3).addTeam(FQ);
+//         bracket.declareWinner(CLG);
+//         bracket.declareWinner(C9);
+//         bracket.declareWinner(FQ);
+//         bracket.declareWinner(NV);
+//         bracket.declareWinner(C9);
+//         bracket.declareWinner(FQ);
+//         bracket.declareWinner(C9);
+//         bracket.go();
+//     }
+// }
--- Tournament.java
+++ Tournament.java
@@ -3,108 +3,252 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;   
 
-public class Tournament {
-    String name;
-    Bracket bracket;    
+public class Tournament extends JFrame {
+    // String name;
+    // Bracket bracket;    
+    // JFrame frame;
+    Bracket bracket;
+    public static int idCounter;
+    int id;
+    JPanel contentContainer, headerContainer, footerContainer;
+    ArrayList<JPanel> roundList = new ArrayList<JPanel>();
+
+    public Color randomColor() {
+        int r = (int) (Math.random() * 250);
+        int g = (int) (Math.random() * 250);
+        int b = (int) (Math.random() * 250);
+        
+        Color color = new Color(r, g, b);
+        return color;
+    }
     
-    public Tournament(String n) {
-        name = n;
-        bracket = new Bracket();
-        // bracket.drawRounds();
-        JFrame frame = new JFrame("Tournament Builder - " + name);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel contentContainer = new JPanel();
-        JPanel contentFooter = new JPanel();
-        SpringLayout springLayout = new SpringLayout();
-        contentContainer.setLayout(springLayout);
-        contentContainer.setBackground(Color.white);
-        contentFooter.setBackground(Color.white);
+    public Tournament(int numRds) {
+        id = ++idCounter;
+        bracket = new Bracket(numRds);
+        contentContainer = new JPanel();
+        headerContainer = new JPanel();
+        footerContainer = new JPanel();
+        GridLayout gridLayout = new GridLayout(1,0);
+    
+        contentContainer.setLayout(gridLayout);
+        contentContainer.setBackground(Color.WHITE);
+        headerContainer.setBackground(Color.WHITE);
+        footerContainer.setBackground(Color.WHITE);
 
-        JButton button = new JButton("Add Round");
+        JButton addButton = new JButton("Add Container");
+        addButton.addActionListener(new AddContainerListener());
+        headerContainer.add(addButton);
+        footerContainer.add(new JButton("Testing3"));
 
-        // ArrayList<JPanel> panels = new ArrayList<JPanel>();
-
-        // int numTeams = 8;
-        // for (int i = numTeams/2; i >= 1; i /= 2) {
-        //     JPanel newPanel = new JPanel();
-        //     newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.PAGE_AXIS));
-        //     newPanel.setBackground(new Color(255-(i*6),235-(i*5),225-(i*4)));
-
-        //     Dimension min = new Dimension(150,0);
-        //     Dimension pref = new Dimension(150,0);
-        //     Dimension max = new Dimension(150, 0);
-        //     newPanel.add(new Box.Filler(min, pref, max));
-           
-        //     newPanel.add(Box.createVerticalGlue());
-        //     for (int j = i; j > 0; j--) {
-        //         JLabel newLabel = new JLabel("New Label: " + (j+1));
-        //         JButton newButton = new JButton("New button: " + (j+1));
-        //         JButton newButtonTwo = new JButton("New button: " + (j));
-        //         newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //         newButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //         newButtonTwo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //         newPanel.add(newLabel);
-        //         newPanel.add(newButton);
-        //         newPanel.add(newButtonTwo);
-        //         if (j > 1) {
-        //             newPanel.add(Box.createVerticalGlue());
-        //         }
-                    
-        //         // newPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-                
-        //     }
-        //     newPanel.add(Box.createVerticalGlue());
-        //     // newPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-        //     // JButton addButton = new JButton("Add Round");
-        //     // addButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //     // newPanel.add(addButton);
-        //     //  if (i == 0) {
-        //     //     System.out.println(newPanel);
-        //     // }
-
-            
-        //     panels.add(newPanel);
+        // for (int i = 0; i < bracket.rounds.size(); i++) {
+        //     roundList.add(new JPanel());
         // }
-        // System.out.println(bracket.panels.get(0));
-        for (int i = 0; i < bracket.panels.size(); i++) {
-            if (i == 0) {
-                springLayout.putConstraint(SpringLayout.WEST, bracket.panels.get(i), 5, SpringLayout.WEST, contentContainer);
-                springLayout.putConstraint(SpringLayout.SOUTH, contentContainer, 5, SpringLayout.SOUTH, bracket.panels.get(i));
-            } else {
-                springLayout.putConstraint(SpringLayout.WEST, bracket.panels.get(i), 5, SpringLayout.EAST, bracket.panels.get(i-1));
-                if (i == (bracket.panels.size()-1)) {
-                    springLayout.putConstraint(SpringLayout.EAST, contentContainer, 5, SpringLayout.EAST, bracket.panels.get(i));
-                    
-                }
-                springLayout.putConstraint(SpringLayout.NORTH, bracket.panels.get(i), -5, SpringLayout.NORTH, contentContainer);
-                springLayout.putConstraint(SpringLayout.SOUTH, bracket.panels.get(i), -5, SpringLayout.SOUTH, contentContainer); 
+        
+
+        for (int i = 0; i < bracket.rounds.size(); i++) {
+            JPanel round = new JPanel();
+            round.setLayout(new BoxLayout(round, BoxLayout.Y_AXIS));
+            round.add(Box.createVerticalGlue());
+            for (int j = 0; j < bracket.rounds.get(i).games.size(); j++) {
+                JPanel game = new JPanel();
+                game.setLayout(new BoxLayout(game, BoxLayout.Y_AXIS));
+                // round.add(Box.createRigidArea(new Dimension(0, 20)));
+                // round.add(Box.createVerticalGlue());
+                JButton teamOne = new JButton(bracket.rounds.get(i).games.get(j).teams.get(0).name);
+                JButton teamTwo = new JButton(bracket.rounds.get(i).games.get(j).teams.get(1).name);
+                // JButton btn = new JButton("Button: " + (j+1));
+                teamOne.addActionListener(new ButtonListener());
+                teamTwo.addActionListener(new ButtonListener());
+                teamOne.setAlignmentX(Component.CENTER_ALIGNMENT);
+                teamTwo.setAlignmentX(Component.CENTER_ALIGNMENT);
+                game.add(Box.createVerticalGlue());
+                game.add(teamOne);
+                game.add(teamTwo);
+                game.add(Box.createVerticalGlue());
+                game.setAlignmentX(Component.CENTER_ALIGNMENT);
+                round.add(Box.createRigidArea(new Dimension(0,5)));
+                round.add(game);
+                round.add(Box.createRigidArea(new Dimension(0,5)));
+                round.add(Box.createVerticalGlue());
             }
-            contentContainer.add(bracket.panels.get(i));
+            
+            // roundList.get(i).setBackground(new Color(200+(i*10),150+(i*15),250-(i*20)));
+            // roundList.get(i).add(btn);
+            round.setBackground(randomColor());
+            roundList.add(round);
+            contentContainer.add(round);
         }
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        contentFooter.add(button);
-        frame.getContentPane().setBackground(new Color(0,0,0));
-        frame.getContentPane().add(BorderLayout.CENTER, contentContainer);
-        frame.getContentPane().add(BorderLayout.SOUTH, contentFooter);
 
-        frame.pack();
-        frame.setVisible(true);
-
+        setTitle("Tournament Builder");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.WHITE);
+        getContentPane().add(headerContainer, BorderLayout.NORTH);
+        getContentPane().add(contentContainer, BorderLayout.CENTER);
+        getContentPane().add(footerContainer, BorderLayout.SOUTH);
+        pack();
+        setVisible(true);
     }
 
-    public void go() {
-        System.out.println(name + " ");
+    class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton button = (JButton) e.getSource();
+            JPanel panel = (JPanel) button.getParent();
+            JPanel container = (JPanel) panel.getParent();
+            // JFrame frame = (JFrame) container.getParent();
+            JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, container);
+            // System.out.println("button clicked: " + frame);
+            roundList.remove(panel);
+            container.remove(panel);
+            // container.revalidate();
+            // container.repaint();
+            frame.revalidate();
+            frame.repaint();
+        }
     }
 
-    public String getName() {
-        return name;
+    class AddContainerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int i = roundList.size();
+            int prevNumGames = bracket.rounds.get(0).games.size();
+
+            JPanel round = new JPanel();
+            BracketRound bracketRound = new BracketRound(prevNumGames * 2);
+            bracket.addRound(bracketRound);
+
+            round.setLayout(new BoxLayout(round, BoxLayout.Y_AXIS));
+            round.add(Box.createVerticalGlue());
+            for (int j = 0; j < bracket.rounds.get(0).games.size(); j++) {
+                JPanel game = new JPanel();
+                game.setLayout(new BoxLayout(game, BoxLayout.Y_AXIS));
+                // round.add(Box.createRigidArea(new Dimension(0, 20)));
+                // round.add(Box.createVerticalGlue());
+                JButton teamOne = new JButton(bracketRound.games.get(j).teams.get(0).name);
+                JButton teamTwo = new JButton(bracketRound.games.get(j).teams.get(1).name);
+                // JButton btn = new JButton("Button: " + (j+1));
+                teamOne.addActionListener(new ButtonListener());
+                teamTwo.addActionListener(new ButtonListener());
+                teamOne.setAlignmentX(Component.CENTER_ALIGNMENT);
+                teamTwo.setAlignmentX(Component.CENTER_ALIGNMENT);
+                game.add(Box.createVerticalGlue());
+                game.add(teamOne);
+                game.add(teamTwo);
+                game.add(Box.createVerticalGlue());
+                game.setAlignmentX(Component.CENTER_ALIGNMENT);
+                round.add(Box.createRigidArea(new Dimension(0,5)));
+                round.add(game);
+                round.add(Box.createRigidArea(new Dimension(0,5)));
+                round.add(Box.createVerticalGlue());
+            }
+            
+            round.setBackground(randomColor());
+            roundList.add(0, round);
+            contentContainer.add(round, 0);
+
+
+
+
+
+
+
+
+
+
+
+            // JButton btn = new JButton("Button: " + (i+1));
+            JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, (JButton) e.getSource());
+            // btn.addActionListener(new ButtonListener());
+            // panel.setBackground(randomColor());
+            // panel.add(btn);
+            // roundList.add(panel);
+            // contentContainer.add(panel);
+            // container.remove(panel);
+            // container.revalidate();
+            // container.repaint();
+            frame.revalidate();
+            frame.repaint();
+        }
     }
+
+
+        // bracket = new Bracket();
+        // bracket.drawRounds();
+        
+        // JFrame frame = new JFrame("Tournament Builder - " + name);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // JPanel contentContainer = new JPanel();
+        // JPanel contentFooter = new JPanel();
+        // JPanel contentHeader = new JPanel();
+        // SpringLayout springLayout = new SpringLayout();
+        // contentContainer.setLayout(springLayout);
+        // contentContainer.setBackground(Color.white);
+        // contentFooter.setBackground(Color.white);
+
+        // JButton addRdButton = new JButton("Add Round");
+        // JButton newTournamentButton = new JButton("New Tournament");
+        // JButton loadTournamentButton = new JButton("Load Tournament");
+
+        // for (int i = 0; i < bracket.panels.size(); i++) {
+        //     if (i == 0) {
+        //         springLayout.putConstraint(SpringLayout.WEST, bracket.panels.get(i), 5, SpringLayout.WEST, contentContainer);
+        //         springLayout.putConstraint(SpringLayout.SOUTH, contentContainer, 5, SpringLayout.SOUTH, bracket.panels.get(i));
+        //     } else {
+        //         springLayout.putConstraint(SpringLayout.WEST, bracket.panels.get(i), 5, SpringLayout.EAST, bracket.panels.get(i-1));
+        //         if (i == (bracket.panels.size()-1)) {
+        //             springLayout.putConstraint(SpringLayout.EAST, contentContainer, 5, SpringLayout.EAST, bracket.panels.get(i));
+                    
+        //         }
+        //         springLayout.putConstraint(SpringLayout.NORTH, bracket.panels.get(i), -5, SpringLayout.NORTH, contentContainer);
+        //         springLayout.putConstraint(SpringLayout.SOUTH, bracket.panels.get(i), -5, SpringLayout.SOUTH, contentContainer); 
+        //     }
+        //     if (i == 0) {
+        //         JButton removeButton = new JButton("Remove Round");
+        //         removeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //         removeButton.addActionListener(new RemoveRoundListener(bracket.rounds.get(i)));
+        //         // newPanel.add(removeButton);
+        //         bracket.panels.get(i).add(removeButton);
+        //     }
+        //     contentContainer.add(bracket.panels.get(i));
+        // }
+        // addRdButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // contentHeader.add(newTournamentButton);
+        // contentHeader.add(loadTournamentButton);
+        // contentFooter.add(addRdButton);
+        // frame.getContentPane().setBackground(new Color(0,0,0));
+        // frame.getContentPane().add(BorderLayout.CENTER, contentContainer);
+        // frame.getContentPane().add(BorderLayout.SOUTH, contentFooter);
+        // frame.getContentPane().add(BorderLayout.NORTH, contentHeader);
+
+        // frame.pack();
+        // frame.setVisible(true);
+
+    
+
+    // public class RemoveRoundListener implements ActionListener {
+    //     private int roundIndex;
+
+    //     public RemoveRoundListener(BracketRound br) {
+    //         roundIndex = bracket.rounds.indexOf(br);
+    //     }
+    //     public void actionPerformed(ActionEvent event) {
+    //         bracket.rounds.remove(roundIndex);
+    //         // frame.repaint();
+
+    //         // drawRounds();
+    //         System.out.println(roundIndex);
+    //         bracket.drawRounds();
+    //         frame.revalidate();
+    //         frame.repaint();
+    //     }
+    // }
+
 }
 
 class TournamentTest {
     public static void main (String[] args) {
-        Tournament t = new Tournament(args[0]);
-        t.go();
+        int numRds = Integer.parseInt(args[0]);
+        Tournament t = new Tournament(numRds);        
+        // t.go();
     }
 }
